6c59df21b6d98c99d8507771965865e08446870f
David Almaguer <david.almaguer@enroutesystems.com>
2019-08-26 17:07:27 -0500

    Macro named differently to avoid syntax warnings

--- JWBestPracticeApps/FeedTableViewController/FeedTableViewController-Bridging-Header.h
+++ JWBestPracticeApps/FeedTableViewController/FeedTableViewController-Bridging-Header.h
@@ -1,14 +1,14 @@
 //
-//  FeedTableViewController-Bridging-Header.h
+//  FeedTableViewController_Bridging_Header.h
 //  FeedTableViewController
 //
 //  Created by David Almaguer on 8/14/19.
 //  Copyright © 2019 Karim Mourra. All rights reserved.
 //
 
-#ifndef FeedTableViewController-Bridging-Header_h
-#define FeedTableViewController-Bridging-Header_h
+#ifndef FeedTableViewController_Bridging_Header_h
+#define FeedTableViewController_Bridging_Header_h
 
 #import <JWPlayer_IOS_SDK/JWPlayerController.h>
 
-#endif /* FeedTableViewController-Bridging-Header_h */
+#endif /* FeedTableViewController_Bridging_Header_h */
