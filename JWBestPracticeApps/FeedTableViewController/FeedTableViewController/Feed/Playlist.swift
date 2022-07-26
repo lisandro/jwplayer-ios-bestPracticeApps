3308c3d175eb8b1b78fdfd5eb7054bb4018634c5
Amitai Blickstein <blickstein@gmail.com>
2022-07-26 13:51:59 -0400

    Removes LoadingCell, FeedItemModel

---
+++ JWBestPracticeApps/FeedTableViewController/FeedTableViewController/Feed/Playlist.swift
@@ -0,0 +1,24 @@
+//
+//  JWV2Playlist.swift
+//  FeedTableViewController
+//
+//  Created by Amitai Blickstein on 6/26/22.
+//
+
+import Foundation
+
+struct Playlist {
+static let 
+    "https://cdn.jwplayer.com/manifests/YuJG5ygn.m3u8",
+    "https://cdn.jwplayer.com/manifests/XKvnATBz.m3u8",
+    "https://cdn.jwplayer.com/manifests/FCEcttZF.m3u8",
+    "https://cdn.jwplayer.com/manifests/M2U5cjk2.m3u8",
+    "https://cdn.jwplayer.com/manifests/Yj1Me5ji.m3u8",
+    "https://cdn.jwplayer.com/manifests/N7thLVIE.m3u8",
+    "https://cdn.jwplayer.com/manifests/SgKzo48g.m3u8",
+    "https://cdn.jwplayer.com/manifests/A95GxtFI.m3u8",
+    "https://cdn.jwplayer.com/manifests/lNjekeid.m3u8",
+    "https://cdn.jwplayer.com/manifests/KLwWokqk.m3u8",
+    "https://cdn.jwplayer.com/manifests/hysZAKhf.m3u8",
+    
+}
