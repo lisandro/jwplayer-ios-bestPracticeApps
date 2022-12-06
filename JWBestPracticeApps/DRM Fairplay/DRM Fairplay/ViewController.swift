77b4734cf60a27bab6db62d93c86e08ebfcb5a8f
David Perez <32963483+DavidPerezP124@users.noreply.github.com>
2022-12-06 15:04:47 -0600

    [SDK-9988] Update DRM BPA to use Studio DRM (#100)
    * [SDK-9988] Update DRM BPA to use Studio DRM
    
    * Fix player config call
    
    * Fix typo add more comments
    
    * Remove unused code in codable struct

--- JWBestPracticeApps/DRM Fairplay/DRM Fairplay/ViewController.swift
+++ JWBestPracticeApps/DRM Fairplay/DRM Fairplay/ViewController.swift
@@ -7,45 +7,83 @@
 
 import UIKit
 import JWPlayerKit
-
-private let EZDRMLicenseAPIEndpoint = "https://fps.ezdrm.com/api/licenses"
-private let EZDRMCertificateEndpoint = "https://fps.ezdrm.com/demo/video/eleisure.cer"
-private let EZDRMVideoEndpoint = "https://fps.ezdrm.com/demo/video/ezdrm.m3u8"
+// For more info on signed URLs: https://docs.jwplayer.com/platform/docs/protection-studio-drm-generate-a-signed-content-url-for-drm-playback
+private let signedURL = "{YOUR_SIGNED_URL}"
 
 class ViewController: JWPlayerViewController,
                       JWDRMContentKeyDataSource {
 
-    var contentUUID: String?
+    /// The locator for the certificate
+    var certificateURLStr: String?
+    /// The server playback context locator
+    /// This is used to request the content key from the Key Server Module
+    var processSPCURLStr: String?
+    // The video URL that we get from decoding the playlist
+    var videoURL: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        createPlaylist(from: URL(string: signedURL)!)
+    }
+    
+    private func createPlaylist(from url: URL) {
+        let urlRequest = URLRequest(url: url)
+        let semaphore = DispatchSemaphore(value: 0)
+        // Signed URLs locate the playlist from JWPlayer's CDN, we then try to decode the playlist into a DRMPlaylist.
+        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, response, error in
+            defer { semaphore.signal() }
+            guard let self = self else {
+                return
+            }
 
-        // Set up the player.
-        setUpPlayer()
+            guard let data = data else {
+                print(String(describing:error?.localizedDescription))
+                return
+            }
+            // Try to decode the response into a DRM playlist.
+            do {
+                let drmPlaylist = try JSONDecoder().decode(DRMPlaylist.self, from: data)
+                // The playlist item has several sources we want to use the m3u8 file, for that we use the first source that has an mpegurl type.
+                guard let source = drmPlaylist.playlist.first?.sources.first(where: { $0.type == "application/vnd.apple.mpegurl"}) else {
+                    return
+                }
+                let drm = source.drm
+                // We get the certificate URL from the Fairplay object
+                self.certificateURLStr = drm.fairplay?.certificateURL
+                // We get the processSPC from the Fairplay object
+                self.processSPCURLStr = drm.fairplay?.processSpcURL
+                self.videoURL = source.file
+            } catch {
+                // Do something with the error.
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+        semaphore.wait()
+        if let video = videoURL, let videoURL = URL(string: video) {
+            self.setUpPlayer(with: videoURL)
+        }
     }
 
     /**
      Sets up the player with a DRM configuration.
      */
-    private func setUpPlayer() {
+    private func setUpPlayer(with videoURL: URL) {
         // Open a do-catch block to catch possible errors with the builders.
         do {
-            let videoUrl = URL(string:EZDRMVideoEndpoint)!
-
             // First, use the JWPlayerItemBuilder to create a JWPlayerItem that will be used by the player configuration.
             let playerItem = try JWPlayerItemBuilder()
-                .file(videoUrl)
+                .file(videoURL)
                 .build()
 
             // Second, create a player config with the created JWPlayerItem.
             let config = try JWPlayerConfigurationBuilder()
-                .playlist([playerItem])
+                .playlist(items: [playerItem])
                 .autostart(true)
                 .build()
 
             // Third, set the data source class. This class conforms to JWDRMContentKeyDataSource, and defines methods which affect DRM.
             player.contentKeyDataSource = self
-
             // Lastly, use the created JWPlayerConfiguration to set up the player.
             player.configurePlayer(with: config)
         } catch {
@@ -61,13 +99,8 @@ class ViewController: JWPlayerViewController,
      When called, this delegate method requests the identifier for the protected content to be passed through the delegate method's completion block.
      */
     func contentIdentifierForURL(_ url: URL, completionHandler handler: @escaping (Data?) -> Void) {
-        guard let uuid = url.absoluteString.split(separator: ";").last,
-              let uuidData = uuid.data(using: .utf8) else {
-            handler(nil)
-            return
-        }
-        self.contentUUID = String(uuid)
-        handler(uuidData)
+        let data = url.host?.data(using: .utf8)
+        handler(data)
     }
 
 
@@ -76,13 +109,17 @@ class ViewController: JWPlayerViewController,
      - note: The Application Certificate must be encoded with the X.509 standard with distinguished encoding rules (DER). It is obtained when registering an FPS playback app with Apple, by supplying an X.509 Certificate Signing Request linked to your private key.
      */
     func appIdentifierForURL(_ url: URL, completionHandler handler: @escaping (Data?) -> Void) {
-        print("url: \(url)")
-        guard let certUrl = URL(string: EZDRMCertificateEndpoint),
-              let appIdData = try? Data(contentsOf: certUrl) else {
-            handler(nil)
+        guard let certificateURLStr = certificateURLStr, let certificateURL = URL(string: certificateURLStr) else {
             return
         }
-        handler(appIdData)
+        let request = URLRequest(url: certificateURL)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, err) in
+            if let err = err {
+                print(err.localizedDescription)
+            }
+            handler(data)
+        }
+        task.resume()
     }
 
     /**
@@ -93,24 +130,20 @@ class ViewController: JWPlayerViewController,
      - note: For resources that may expire, specify a renewal date and the content-type in the completion block.
      */
     func contentKeyWithSPCData(_ spcData: Data, completionHandler handler: @escaping (Data?, Date?, String?) -> Void) {
-        guard let contentUUID = self.contentUUID else {
-            handler(nil, nil, nil)
+        guard let processSPCURLStr = processSPCURLStr, let processSPCURL = URL(string: processSPCURLStr) else {
             return
         }
+        var request = URLRequest(url: processSPCURL)
+        request.httpMethod = "POST"
+        request.addValue("application/octet-stream", forHTTPHeaderField: "Content-type")
+        request.httpBody = spcData
 
-        let currentTime = Date().timeIntervalSince1970
-        let licenseApiPath = EZDRMLicenseAPIEndpoint.appendingFormat("/%@?p1=%li", contentUUID, currentTime)
-        var ckcRequest = URLRequest(url: URL(string: licenseApiPath)!)
-        ckcRequest.httpMethod = "POST"
-        ckcRequest.httpBody = spcData
-        ckcRequest.addValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
-
-        URLSession.shared.dataTask(with: ckcRequest) { (data, response, error) in
-            guard error == nil, let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                handler(nil, nil, nil)
-                return
+        let task = URLSession.shared.dataTask(with: request) { (data, response, err) in
+            if let err = err {
+                print(err.localizedDescription)
             }
-            handler(data, nil, nil)
-        }.resume()
+            handler(data, nil, "application/octet-stream")
+        }
+        task.resume()
     }
 }
