e59a9c4c5761bcbc6cf39419d3239ae4cd81d1c5
David Almaguer <53494136+david-almaguer@users.noreply.github.com>
2022-12-12 16:33:34 -0600

    [SDK-10583] Standardize iOS 12 deployment target (#102)
    * Sets projects Xcode minimum compatibility version to 13.0
    
    * Sets pods platform to iOS 12.0
    
    * Updates BPA projects to be compatible with iOS 12+
    
    * Adds missing shared schemas
    
    * Updates global settings and compatibility with iOS 12 for some projects

--- JWBestPracticeApps/AdvancedPlayer/AdvancedPlayer/ViewController.swift
+++ JWBestPracticeApps/AdvancedPlayer/AdvancedPlayer/ViewController.swift
@@ -142,7 +142,12 @@ class ViewController: JWPlayerViewController,
         super.jwplayer(player, didPauseWithReason: reason)
         // Set the play/pause button image on the main thread
         DispatchQueue.main.async { [weak self] in
-            self?.customControls.playPauseButton.setImage(UIImage(systemName: "play"), for: .normal)
+            if #available(iOS 13.0, *) {
+                self?.customControls.playPauseButton.setImage(UIImage(systemName: "play"), for: .normal)
+            } else {
+                // Fallback on earlier versions
+                self?.customControls.playPauseButton.setImage(UIImage(named: "play.fill"), for: .normal)
+            }
         }
     }
 
@@ -150,7 +155,12 @@ class ViewController: JWPlayerViewController,
         super.jwplayer(player, isPlayingWithReason: reason)
         // Set the play/pause button image on the main thread
         DispatchQueue.main.async { [weak self] in
-            self?.customControls.playPauseButton.setImage(UIImage(systemName: "pause"), for: .normal)
+            if #available(iOS 13.0, *) {
+                self?.customControls.playPauseButton.setImage(UIImage(systemName: "pause"), for: .normal)
+            } else {
+                // Fallback on earlier versions
+                self?.customControls.playPauseButton.setImage(UIImage(named: "pause.fill"), for: .normal)
+            }
         }
     }
 
@@ -213,11 +223,21 @@ class ViewController: JWPlayerViewController,
             }
         case .play:
             DispatchQueue.main.async { [weak self] in
-                self?.customControls.playPauseButton.setImage(UIImage(systemName: "pause"), for: .normal)
+                if #available(iOS 13.0, *) {
+                    self?.customControls.playPauseButton.setImage(UIImage(systemName: "pause"), for: .normal)
+                } else {
+                    // Fallback on earlier versions
+                    self?.customControls.playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+                }
             }
         case .pause:
             DispatchQueue.main.async { [weak self] in
-                self?.customControls.playPauseButton.setImage(UIImage(systemName: "play"), for: .normal)
+                if #available(iOS 13.0, *) {
+                    self?.customControls.playPauseButton.setImage(UIImage(systemName: "play"), for: .normal)
+                } else {
+                    // Fallback on earlier versions
+                    self?.customControls.playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+                }
             }
         case .skipped:
             DispatchQueue.main.async { [weak self] in
