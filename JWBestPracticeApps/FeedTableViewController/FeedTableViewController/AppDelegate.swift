0c1f131734c1570d15a6409a60a14d0a4430811b
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2025-07-04 11:38:45 -0300

    chore: Extract license key to a constant to force set the value

--- JWBestPracticeApps/FeedTableViewController/FeedTableViewController/AppDelegate.swift
+++ JWBestPracticeApps/FeedTableViewController/FeedTableViewController/AppDelegate.swift
@@ -14,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         #warning("Add your JW Player license key.")
-        // JWPlayerKitLicense.setLicenseKey(<#key: String#>)
-
+        JWPlayerKitLicense.setLicenseKey(licenseKey)
+        
         return true
     }
 
